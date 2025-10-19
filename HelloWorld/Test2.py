806561cbfd47bc0af8be04ff7e9f4af92503740d
Hithat26 <hitanshjgada17@gmail.com>
2025-10-19 19:20:33 +0530

    Variables

---
+++ HelloWorld/Test2.py
@@ -0,0 +1,5 @@
+# Ask two questions: person's name and favourite color.
+# Then print message like "Mosh likes blue".
+name = input('What is your name? ')
+color = input('What is your favourite color? ')
+print(name + ' likes ' + color)
