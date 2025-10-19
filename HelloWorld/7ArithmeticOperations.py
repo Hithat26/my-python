a2e42a28f21b33e26754d60e78996b62f1d37c65
Hithat26 <hitanshjgada17@gmail.com>
2025-10-19 19:49:41 +0530

    Arithmetic Operations in python

---
+++ HelloWorld/7ArithmeticOperations.py
@@ -0,0 +1,15 @@
+# Different Arithmetic Operators
+# Addition
+print(10 + 3)
+# Subtraction
+print(10 - 3)
+# Multiplication
+print(10 * 3)
+# Division returning a Floating Point Number
+print(10 / 3)
+# Division returning an Integer
+print(10 // 3)
+# Remainder / Modulus operator
+print(10 % 3)
+# Exponent
+print(10 ** 3)
