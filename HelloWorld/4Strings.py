4964b74090791e1da0209372f533ca67c9e19c4f
Hithat26 <hitanshjgada17@gmail.com>
2025-10-19 19:19:40 +0530

    Strings in python

---
+++ HelloWorld/4Strings.py
@@ -0,0 +1,22 @@
+course = "Python's course for beginners"
+# print(course)
+# course = 'Python course for "beginners"'
+# print(course)
+# course = '''
+#
+# Hi John,
+#
+# Here is our first email to you.
+#
+# Thank you,
+# Hitansh
+# '''
+# print(course)
+# print(course[0]) # First character
+# print(course[-1]) # Last character
+# print(course[1:3]) # prints characters from 1 to 3 except character at 3
+# print(course[:3]) # prints characters till 3
+# print(course[3:]) # prints characters from 3
+
+another = course[:] # creates a copy of it
+print(another)
