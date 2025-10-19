332d6e392ac41e23d55d4e359570cabf56100f50
Hithat26 <hitanshjgada17@gmail.com>
2025-10-19 19:42:33 +0530

    String Methods in python

---
+++ HelloWorld/6StringMethods.py
@@ -0,0 +1,14 @@
+course = 'Python for Beginners'
+print(course)
+
+print(len(course)) # len() function
+# Methods of strings
+print(course.upper())
+print(course.lower())
+print(course.find('o'))
+print(course.find('O'))
+print(course.find('Beginners'))
+print(course.replace('Beginners', 'Absolute Beginners'))
+print(course.replace('P', 'J'))
+print('Python' in course)
+print('python' in course)
