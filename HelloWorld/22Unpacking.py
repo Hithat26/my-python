79f9ff3f34421f3c2fb55d4258d4fc0669b8dd3e
Hithat26 <hitanshjgada17@gmail.com>
2025-10-21 14:48:30 +0530

    Unpacking in python

---
+++ HelloWorld/22Unpacking.py
@@ -0,0 +1,10 @@
+coordinates = (1, 2, 3)
+# x = coordinates[0]
+# y = coordinates[1]
+# z = coordinates[2]
+# The same above can be done by Unpacking
+x, y, z = coordinates
+print(x)
+print(y)
+print(z)
+# Same can be done with Lists
